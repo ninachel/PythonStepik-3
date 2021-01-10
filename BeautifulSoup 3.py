57fe8f0b2b0113305f71878d8cc401af3ed3cf93
ninachel <nccccc17@gmail.com>
2021-01-11 00:42:00 +0300

    -

---
+++ BeautifulSoup 3.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen, urlretrieve
+from bs4 import BeautifulSoup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+resp = urlopen('https://stepik.org/media/attachments/lesson/209723/4.html') # скачиваем файл
+html = resp.read().decode('utf8') # считываем содержимое
+soup = BeautifulSoup(html, 'html.parser') # делаем суп
+result = 0
+for tag in soup.find_all('td'):
+    result += int(tag.string)
+print(result)
