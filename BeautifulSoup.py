a5e4d88364f6dc471bb8c4eff8e1318f0ff77eec
ninachel <nccccc17@gmail.com>
2021-01-11 00:26:55 +0300

    -

---
+++ BeautifulSoup.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen, urlretrieve
+from bs4 import BeautifulSoup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+resp = urlopen('https://stepik.org/media/attachments/lesson/245130/6.html') # скачиваем файл
+html = resp.read().decode('utf8') # считываем содержимое
+soup = BeautifulSoup(html, 'html.parser') # делаем суп
+table = soup.find('table', attrs = {'class' : 'wikitable sortable'})
+cnt = 0
+for tr in soup.find_all('tr'):
+    cnt += 1
+    for td in tr.find_all(['td', 'th']):
+        cnt *= 2
+print(cnt)
